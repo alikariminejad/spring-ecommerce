bab664f11abab7c62aeb2c9ea18bcea08eb6add7
Ali Kariminejad <kariminejad.ali.ak@gmail.com>
2026-05-11 17:50:48 +0330

    Challenge Update Category

--- sb-ecom/src/main/java/com/ecommerce/project/controller/CategoryController.java
+++ sb-ecom/src/main/java/com/ecommerce/project/controller/CategoryController.java
@@ -38,4 +38,13 @@ public class CategoryController {
         }
 
     }
+    @PutMapping("/api/public/categories/{categoryId}")
+    public ResponseEntity<String> updateCategory(@RequestBody Category category, @PathVariable Long categoryId){
+        try {
+            Category updateCategory = categoryService.updateCategory(category, categoryId);
+            return new ResponseEntity<>("Category with id=" + categoryId + " updated.",HttpStatus.OK);
+        }catch (ResponseStatusException e){
+            return new ResponseEntity<>(e.getReason(), e.getStatusCode());
+        }
+    }
 }
