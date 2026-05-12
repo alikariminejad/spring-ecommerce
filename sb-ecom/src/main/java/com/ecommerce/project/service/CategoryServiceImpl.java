6991a8fdbc398fe8a661647bdab9816c29883070
Ali Kariminejad <kariminejad.ali.ak@gmail.com>
2026-05-12 09:53:36 +0330

    debugged and tested

--- sb-ecom/src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
+++ sb-ecom/src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
@@ -13,9 +13,7 @@ import java.util.Optional;
 
 @Service
 public class CategoryServiceImpl implements CategoryService{
-//    private List<Category> categories = new ArrayList<>();
 
-    private Long nextId = 1L;
     @Autowired
     private CategoryRepository categoryRepository;
 
@@ -26,7 +24,6 @@ public class CategoryServiceImpl implements CategoryService{
 
     @Override
     public void createCategory(Category category) {
-        category.setCategoryId(nextId++);
         categoryRepository.save(category);
     }
 
