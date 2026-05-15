7cc823dea961a74b5ab50cbb7da43e40dc88bdf6
Ali Kariminejad <kariminejad.ali.ak@gmail.com>
2026-05-15 23:46:04 +0330

    One to Many AND Many to One Relationship

--- media/src/main/java/com/social/media/models/SocialUser.java
+++ media/src/main/java/com/social/media/models/SocialUser.java
@@ -2,6 +2,9 @@ package com.social.media.models;
 
 import jakarta.persistence.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 public class SocialUser {
     @Id
@@ -11,5 +14,8 @@ public class SocialUser {
     @OneToOne(mappedBy = "user")
     //@JoinColumn(name = "social_profile_id")
     private SocialProfile socialProfile;
+
+    @OneToMany(mappedBy = "socialUser")
+    private List<Post> posts = new ArrayList<>();
 
 }
