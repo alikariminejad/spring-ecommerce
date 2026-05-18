dcc09610600da4782f439b840a050cc03bdc5ca5
Ali Kariminejad <kariminejad.ali.ak@gmail.com>
2026-05-18 19:16:40 +0330

    Get Products by Keyword

--- sb-ecom/src/main/java/com/ecommerce/project/repositories/ProductRepository.java
+++ sb-ecom/src/main/java/com/ecommerce/project/repositories/ProductRepository.java
@@ -2,6 +2,7 @@ package com.ecommerce.project.repositories;
 
 import com.ecommerce.project.model.Category;
 import com.ecommerce.project.model.Product;
+import com.ecommerce.project.payload.ProductResponse;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,4 +11,5 @@ import java.util.List;
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
     List<Product> findByCategoryOrderByPriceAsc(Category category);
+    List<Product> findByProductNameLikeIgnoreCase(String keyword);
 }
