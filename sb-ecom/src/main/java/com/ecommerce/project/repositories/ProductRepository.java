38ccb7d185b716f28d0f7a85540e8f0eb3d11c46
Ali Kariminejad <kariminejad.ali.ak@gmail.com>
2026-05-19 23:09:15 +0330

    Implementing Pagination and Sorting

--- sb-ecom/src/main/java/com/ecommerce/project/repositories/ProductRepository.java
+++ sb-ecom/src/main/java/com/ecommerce/project/repositories/ProductRepository.java
@@ -2,7 +2,8 @@ package com.ecommerce.project.repositories;
 
 import com.ecommerce.project.model.Category;
 import com.ecommerce.project.model.Product;
-import com.ecommerce.project.payload.ProductResponse;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,6 +11,6 @@ import java.util.List;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
-    List<Product> findByCategoryOrderByPriceAsc(Category category);
-    List<Product> findByProductNameLikeIgnoreCase(String keyword);
+    Page<Product> findByCategoryOrderByPriceAsc(Category category, Pageable pageDetails);
+    Page<Product> findByProductNameLikeIgnoreCase(String keyword, Pageable pageDetails);
 }
