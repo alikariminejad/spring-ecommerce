689889beac7a73e297ee14ea86e3d74f63b018cd
Ali Kariminejad <kariminejad.ali.ak@gmail.com>
2026-05-23 23:53:51 +0330

    Testing Changes

--- sb-ecom/src/main/java/com/ecommerce/project/repositories/RoleRepository.java
+++ sb-ecom/src/main/java/com/ecommerce/project/repositories/RoleRepository.java
@@ -4,7 +4,6 @@ import com.ecommerce.project.model.AppRole;
 import com.ecommerce.project.model.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.lang.ScopedValue;
 import java.util.Optional;
 
 public interface RoleRepository extends JpaRepository<Role, Long> {
