700cbedd8366ed7768ae608a3c0a017aca57d083
Ali Kariminejad <kariminejad.ali.ak@gmail.com>
2026-05-27 09:44:58 +0330

    signOut endpoint

--- sb-ecom/src/main/java/com/ecommerce/project/controller/AuthController.java
+++ sb-ecom/src/main/java/com/ecommerce/project/controller/AuthController.java
@@ -146,4 +146,12 @@ public class AuthController {
         return ResponseEntity.ok()
                 .body(response);
     }
+
+    @PostMapping("/signout")
+    public ResponseEntity<?> signOutUser(){
+        ResponseCookie cookie = jwtUtils.getCleanJwtCookie();
+        return ResponseEntity.ok().header(HttpHeaders.SET_COOKIE,
+                cookie.toString())
+                .body(new MessageResponse("You've been signed out!"));
+    }
 }
