8e4c5d23e5a9c5b05ecf17b23195fabb00821db8
Ali Kariminejad <kariminejad.ali.ak@gmail.com>
2026-06-09 12:01:57 +0330

    Different swagger annotations

--- sb-ecom/src/main/java/com/ecommerce/project/controller/CategoryController.java
+++ sb-ecom/src/main/java/com/ecommerce/project/controller/CategoryController.java
@@ -4,6 +4,12 @@ import com.ecommerce.project.config.AppConstants;
 import com.ecommerce.project.payload.CategoryDTO;
 import com.ecommerce.project.payload.CategoryResponse;
 import com.ecommerce.project.service.CategoryService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -12,6 +18,7 @@ import org.springframework.web.bind.annotation.*;
 
 
 @RestController
+@Tag(name = "Category APIs", description = "APIs for managing categories")
 @RequestMapping("/api")
 public class CategoryController {
 
@@ -28,6 +35,12 @@ public class CategoryController {
         return new ResponseEntity<>(categoryResponse, HttpStatus.OK);
     }
 
+    @Operation(summary = "Create category", description = "API to create a new category")
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", description = "Category is created successfully"),
+            @ApiResponse(responseCode = "400", description = "Invalid Input", content = @Content),
+            @ApiResponse(responseCode = "500", description = "Internal server error", content = @Content)
+    })
     @PostMapping("/public/categories")
     public ResponseEntity<CategoryDTO> createCategory(@Valid @RequestBody CategoryDTO categoryDTO){
         CategoryDTO savedCategoryDTO = categoryService.createCategory(categoryDTO);
@@ -35,7 +48,7 @@ public class CategoryController {
     }
 
     @DeleteMapping("/admin/categories/{categoryId}")
-    public ResponseEntity<CategoryDTO> deleteCategory(@PathVariable  Long categoryId){
+    public ResponseEntity<CategoryDTO> deleteCategory(@Parameter(description = "ID of the category you wish to delete") @PathVariable  Long categoryId){
         CategoryDTO deleteCategory = categoryService.deleteCategory(categoryId);
         return new ResponseEntity<>(deleteCategory, HttpStatus.OK);
 
