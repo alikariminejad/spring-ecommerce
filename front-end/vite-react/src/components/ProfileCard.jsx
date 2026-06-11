b6a234cb4294615ebefe430586cb39473ac02b26
Ali Kariminejad <kariminejad.ali.ak@gmail.com>
2026-06-11 11:18:21 +0330

    Destructuring Props

--- front-end/vite-react/src/components/ProfileCard.jsx
+++ front-end/vite-react/src/components/ProfileCard.jsx
@@ -1,10 +1,16 @@
-function ProfileCard(props) {
-    const name = "Ali";
+function ProfileCard({name, age, isMember}) {
+    // // without destructuring, using Dot notation
+    // const person = { name: "Ali", age: 28 }
+    // const name = person.name;
+    // const age = person.age;
+    
+    // // with destructuring
+    // const { name, age } = person;
     return (
         <div>
-            <h3>Name: {props.name}</h3>
-            <h3>Age: {props.age}</h3>
-            <h3>Staus: {props.isMember ? "Active member" : "Guest"}</h3>
+            <h3>Name: {name}</h3>
+            <h3>Age: {age}</h3>
+            <h3>Staus: {isMember ? "Active member" : "Guest"}</h3>
         </div>
     );
 }
