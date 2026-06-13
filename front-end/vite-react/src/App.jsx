eef8ca08e9c877ebe407bfb8290b6574c698db2b
Ali Kariminejad <kariminejad.ali.ak@gmail.com>
2026-06-13 10:16:03 +0330

    Setting State in Functional Components with React Hooks

--- front-end/vite-react/src/App.jsx
+++ front-end/vite-react/src/App.jsx
@@ -1,39 +1,21 @@
 import './App.css'
-import ProfileCard from './components/ProfileCard';
-// import About from './components/About';
-// import Contact from './components/Contact';
-// import Footer from './components/Footer';
-// import Header from './components/Header';
-// import ProjectList from './components/ProjectList';
+import { useState } from 'react';
 
 function App() {
-  const hobby1 = ['Reading', 'Cooking'];
-  const hobby2 = ['Hiking', 'Swimming'];
-  const handleHobbyClick = (hobby) => {
-    alert(`You clicked on: ${hobby}`);
+  const [count, setCount] = useState(0);
+  const incrementCount = () => {
+    setCount(count + 1);
   };
-
-  const aliProfile = {
-    name:"ALI",
-    age:28,
-    isMember:true,
-    hobbies:hobby1,
-    onHobbyClick:handleHobbyClick
-  }
-  const jackProfile = {
-    name:"Jack",
-    age:29,
-    isMember:false,
-    hobbies:hobby2,
-    onHobbyClick:handleHobbyClick
-  }
+  const decrementCount = () => {
+    setCount(count - 1);
+  };
 
   return (
     <div className="App">
       <div>
-        <h1>Hello</h1>
-        <ProfileCard {...aliProfile} />
-        <ProfileCard {...jackProfile} />
+        <h1>Counter Value: {count}</h1>
+        <button onClick={incrementCount}>Increment</button>
+        <button onClick={decrementCount}>Decrement</button>
       </div>
     </div>
   );
