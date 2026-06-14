d14473b8fda47a08c4f6f00a39ef2be10f857446
Ali Kariminejad <kariminejad.ali.ak@gmail.com>
2026-06-14 23:47:53 +0330

    Understanding the Importance of the Dependency Array

--- front-end/vite-react/src/App.jsx
+++ front-end/vite-react/src/App.jsx
@@ -3,17 +3,17 @@ import { useEffect, useState } from 'react';
 
 function App() {
   const [count, setCount] = useState(0);
+  const [anotherValue, setAnotherValue] = useState(0);
   useEffect(() => {
-    document.title = `Count: ${count + 1}`;
-  }, [count])
+    document.title = `Count: ${count + 1} | Another: ${anotherValue}`;
+    console.log(`useEffect triggered`);
+  })
 
-  const incrementCount = () => {
-    setCount(count + 1);
-  };
   return (
     <div className='App'>
       <h1>useEffect Hook</h1>
-      <button onClick={incrementCount}>Increment</button>
+      <button onClick={()=>setCount(count+1)}>Increment</button>
+      <button onClick={()=>setAnotherValue(anotherValue+1)}>Increment value</button>
     </div>
   );
 }
