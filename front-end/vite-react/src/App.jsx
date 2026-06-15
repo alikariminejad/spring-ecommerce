e8bedf9352684b73cd421ed09dd9fcdeafbec1e4
Ali Kariminejad <kariminejad.ali.ak@gmail.com>
2026-06-15 16:08:32 +0330

    Project Creating a Digital Clock

--- front-end/vite-react/src/App.jsx
+++ front-end/vite-react/src/App.jsx
@@ -1,10 +1,26 @@
 import './App.css'
 import { useEffect, useState } from 'react';
-import ParentComponent from './ParentComponent';
 
 function App() {
-  return (
-    <ParentComponent />
+
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const timerId = setInterval(() => { setTime(new Date()) }, 1000);
+    return () => clearInterval(timerId);
+  }, []);
+  
+  const formattedTime = time.toLocaleTimeString('en-US',
+      {hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit'
+      }
+  )
+  return ( 
+    <div className='clock-container'>
+      <div className='clock'>{formattedTime}</div>
+      
+    </div>
   );
 }
 
