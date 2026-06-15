f9b8fb5ba440b0725e7a90130b0a7d585ebe59e8
Ali Kariminejad <kariminejad.ali.ak@gmail.com>
2026-06-15 15:43:10 +0330

    Using Cleanup Functions with React's useEffect Hook

--- front-end/vite-react/src/App.jsx
+++ front-end/vite-react/src/App.jsx
@@ -1,20 +1,10 @@
 import './App.css'
 import { useEffect, useState } from 'react';
+import ParentComponent from './ParentComponent';
 
 function App() {
-  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
-  useEffect(() => {
-    const handleMouseMove = (event) => {
-      setMousePosition({ x: event.clientX, y: event.clientY });
-    };
-    window.addEventListener('mousemove', handleMouseMove);
-   }, []);
-
   return (
-    <div className='App'>
-      <h2>Mouse Position</h2>
-      <p>X: {mousePosition.x}, Y: {mousePosition.y}</p>
-    </div>
+    <ParentComponent />
   );
 }
 
