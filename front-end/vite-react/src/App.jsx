e16c2967af8db098de8d471a38463632e2a82a99
Ali Kariminejad <kariminejad.ali.ak@gmail.com>
2026-06-20 23:32:15 +0330

    Updating a Custom Hook

--- front-end/vite-react/src/App.jsx
+++ front-end/vite-react/src/App.jsx
@@ -3,13 +3,14 @@ import React from 'react';
 import useCounter from './components/useCounter';
 
 function App() {
-  const { count, increment, decrement } = useCounter();
+  const { count, increment, decrement, reset } = useCounter(3);
 
   return ( 
     <div>
       <h2>Count: {count}</h2>
       <button onClick={increment}>Increment</button>
       <button onClick={decrement}>Decrement</button>
+      <button onClick={reset}>Reset</button>
     </div>
   );
 }
