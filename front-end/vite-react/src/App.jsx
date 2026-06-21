c9f821e0759594fe478cf037d315b9dc8761b116
Ali Kariminejad <kariminejad.ali.ak@gmail.com>
2026-06-21 12:49:48 +0330

    Working with APIs in React

--- front-end/vite-react/src/App.jsx
+++ front-end/vite-react/src/App.jsx
@@ -1,16 +1,26 @@
 import './App.css'
-import React from 'react';
-import useCounter from './components/useCounter';
+import React, { useEffect, useState } from 'react';
 
 function App() {
-  const { count, increment, decrement, reset } = useCounter(3);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(response => response.json())
+      .then(json => setData(json));
+  }, [])
 
   return ( 
     <div>
-      <h2>Count: {count}</h2>
-      <button onClick={increment}>Increment</button>
-      <button onClick={decrement}>Decrement</button>
-      <button onClick={reset}>Reset</button>
+      <h2>API's</h2>
+      <ul>
+        {data.map((post) => (
+          <li key={post.id}>
+            <p><strong>{post.title}</strong></p>
+            <p>{post.body}</p>
+          </li>
+        ))}
+        </ul>
     </div>
   );
 }
