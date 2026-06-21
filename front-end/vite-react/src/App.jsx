8bc8c4e9e20e79b29dc928326cf55db587a3df74
Ali Kariminejad <kariminejad.ali.ak@gmail.com>
2026-06-21 16:46:45 +0330

    Handling Loading and Errors

--- front-end/vite-react/src/App.jsx
+++ front-end/vite-react/src/App.jsx
@@ -3,12 +3,30 @@ import React, { useEffect, useState } from 'react';
 
 function App() {
   const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
+    setLoading(true);
     fetch('https://jsonplaceholder.typicode.com/posts')
       .then(response => response.json())
-      .then(json => setData(json));
+      .then(json => {
+        setData(json);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error(`Error fetching data: `, error);
+        setError('Failed to fetch the data');
+        setLoading(false);
+      });
   }, [])
+
+  if (loading) {
+    return <p>Loading....</p>
+  }
+  if (error) {
+    return <p>{error}</p>
+  }
 
   return ( 
     <div>
