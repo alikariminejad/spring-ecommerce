994286e9d07d55ffab5ad492465b3e1ea8befd31
Ali Kariminejad <kariminejad.ali.ak@gmail.com>
2026-06-22 13:44:51 +0330

    Sending POST Requests

--- front-end/vite-react/src/App.jsx
+++ front-end/vite-react/src/App.jsx
@@ -3,47 +3,29 @@ import './App.css'
 import React, { useEffect, useState } from 'react';
 
 function App() {
-  const [data, setData] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
+  const data, setData = useState();
 
-  useEffect(() => {
-    setLoading(true);
-    axios.all([
-      axios.get('https://jsonplaceholder.typicode.com/posts'),
-      axios.get('https://jsonplaceholder.typicode.com/users'),
-    ])
-      .then(axios.spread((posts, users) => {
-        console.log(posts);
-        console.log(users);
-        setData(posts);
-        setLoading(false);
-      }))
-      .catch((error) => {
-        console.error(`Error fetching data: `, error);
-        setError('Failed to fetch the data');
-        setLoading(false);
-      });
-  }, [])
-
-  if (loading) {
-    return <p>Loading....</p>
-  }
-  if (error) {
-    return <p>{error}</p>
-  }
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const newPost = {
+      title: 'foo',
+      body: 'bar',
+      userId: 1
+    }
+    axios.post('https://jsonplaceholder.typicode.com/posts', newPost).then(
+      response => {
+        console.log('New Post Added: ', response.data);
+        setData([response.data]);
+      }
+    )
+  };
 
   return ( 
     <div>
       <h2>API's</h2>
-      <ul>
-        {data.map((post) => (
-          <li key={post.id}>
-            <p><strong>{post.title}</strong></p>
-            <p>{post.body}</p>
-          </li>
-        ))}
-        </ul>
+      <form onSubmit={handleSubmit}>
+        <button type='submit'>Add Post</button>
+      </form>
     </div>
   );
 }
