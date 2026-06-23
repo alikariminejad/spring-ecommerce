3b1862266c6ceb37aadedaa2c2acf6ceb78a737d
Ali Kariminejad <kariminejad.ali.ak@gmail.com>
2026-06-23 17:20:07 +0330

    Introduction to React Hook Form

--- front-end/vite-react/src/App.jsx
+++ front-end/vite-react/src/App.jsx
@@ -1,60 +1,25 @@
 import './App.css'
 import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
 
 
 function App() {
-  const [formData, setFormData] = useState({
-    name: '',
-    email:''
-  });
-  const [errors, setErrors] = useState({});
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data) => console.log(data);
 
-  const validate = () => {
-    const newErrors = {};
-    if (!formData.name.trim()) {
-      newErrors.name = 'Name is required';
-    }
-    return newErrors;
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const validationErrors = validate();
-    if (Object.keys(validationErrors).length > 0) {
-      setErrors(validationErrors);
-    } else {
-      console.log("Form data submitted ", formData);      
-    }
-  };
-
-  const handleChange = (event) => {
-    const { name, value } = event.target;
-    setFormData({
-      ...formData, [event.target.name]: event.target.value,
-    })
-    if (errors[name]) {
-      const newErrors = { ...errors };
-      delete newErrors[name];
-      setErrors(newErrors);
-    }
-  };
 
   return ( 
     <div>
       <h2>Forms in React</h2>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <label>
-          Name: 
-          <input type="text" name='name' value={formData.name} onChange={handleChange} />
-          {errors.name && <span style={{
-            color:'red'
-          }}>{errors.name}</span>}
-          <br/>
+          Name:
+          <input {...register('name')} />
         </label>
-        
+      
         <label>
           Email: 
-          <input type="email" name='email' value={formData.email} onChange={handleChange} />
+          <input {...register('email')} />
         </label>
 
         <button type='submit'>submit</button>
