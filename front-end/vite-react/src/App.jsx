a84b6525ac7baddbada5da552a761b6701caf3ca
Ali Kariminejad <kariminejad.ali.ak@gmail.com>
2026-06-23 19:01:41 +0330

    Implementing Advanced Custom Validations

--- front-end/vite-react/src/App.jsx
+++ front-end/vite-react/src/App.jsx
@@ -4,10 +4,16 @@ import { useForm } from 'react-hook-form';
 
 
 function App() {
-  const { register, handleSubmit, watch,reset, formState:{errors}} = useForm();
+  const { register, handleSubmit, watch, reset, formState: { errors } } = useForm({mode:'onChange'});
   const onSubmit = (data) => {
-    console.log(data); 
+    console.log(data);
     reset();
+  };
+
+  const existingUsernames = ['admin', 'user123', 'john']
+  const checkIfUsernameExist = async (username) => {
+    await new Promise(resolve => setTimeout(resolve, 1000));
+    return existingUsernames.includes(username);
   };
 
   // const validateName = (value) => {
@@ -39,7 +45,11 @@ function App() {
             minLength: { value: 2, message: "Name should be at least 2 characters" },
             validate: {
               notAdmin: (value) => value !== "admin" || "Admin is not allowd",
-              isNotNumber: (value)=> isNaN(value) || "Number is not allowed"
+              isNotNumber: (value) => isNaN(value) || "Number is not allowed",
+              checkUsername: async(value) => {
+                const exist = await checkIfUsernameExist(value);
+                return !exist || 'Username already taken'
+              }
             }
           })} />
         </label>
@@ -51,8 +61,25 @@ function App() {
         </label>
         {errors.email && <p>Email is required</p>}
 
+        <label>Password:
+          <input type="password" {...register('password', { required: true })} />
+        </label>
+        {errors.password && <p>{errors.password.message}</p>}
+        <br/>
+        <label>Confirm Password:
+          <input type="password" {...register('confirmPassword', {
+            required: true,
+            minLength: 2,
+            validate: value => value === watch('password') || 'Passwords do not match'
+          })} />
+        </label>
+        {errors.confirmPassword && errors.confirmPassword.message}
+
+        <br/>
         <button type='submit'>submit</button>
-        <button type='button' onClick={()=>reset()}>reset</button>
+        <button type='button' onClick={() => reset()}>reset</button>
+        
+
       </form>
     </div>
   );
