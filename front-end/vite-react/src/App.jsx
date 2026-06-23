48185d2a4b85288eab300d8537f4ef40cbcb884b
Ali Kariminejad <kariminejad.ali.ak@gmail.com>
2026-06-23 16:27:13 +0330

    Introduction to Forms in React

--- front-end/vite-react/src/App.jsx
+++ front-end/vite-react/src/App.jsx
@@ -1,32 +1,39 @@
-import axios from 'axios';
 import './App.css'
-import React, { useEffect, useState } from 'react';
-import api from './api/api';
+import React, { useState } from 'react';
 
 
 function App() {
-  const [data, setData] = useState();
+  const [formData, setFormData] = useState({
+    name: '',
+    email:''
+  });
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const newPost = {
-      title: 'foo',
-      body: 'bar',
-      userId: 1
-    }
-    api.post('/posts', newPost).then(
-      response => {
-        console.log('New Post Added: ', response.data);
-        setData([response.data]);
-      }
-    )
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Form data submitted ", formData);
+  };
+
+  const handleChange = (evenet) => {
+    setFormData({
+      ...formData, [event.target.name]: evenet.target.value,
+    })
   };
 
   return ( 
     <div>
-      <h2>API's</h2>
+      <h2>Forms in React</h2>
       <form onSubmit={handleSubmit}>
-        <button type='submit'>Add Post</button>
+        <label>
+          Name: 
+          <input type="text" name='name' value={formData.name} onChange={handleChange} />
+        </label>
+        
+        <label>
+          Email: 
+          <input type="email" name='email' value={formData.email} onChange={handleChange} />
+        </label>
+
+        <button type='submit'>submit</button>
       </form>
     </div>
   );
