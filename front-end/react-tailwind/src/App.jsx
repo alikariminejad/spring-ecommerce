16d0844a66bdae24c7e2aefcacc2b5202ec25836
Ali Kariminejad <kariminejad.ali.ak@gmail.com>
2026-06-27 11:43:40 +0330

    Implementing Dark Mode using Tailwind CSS

--- front-end/react-tailwind/src/App.jsx
+++ front-end/react-tailwind/src/App.jsx
@@ -1,19 +1,15 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
+import DarkModeToggle from './DarkModeToggle'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className='bg-gray-100 p-4'>
-      <ui className='list-none md:flex md:gap-4'>
-        <li className='bg-blue-500 text-white p-2 m-2 rounded-lg md:px-4 '>Item 1</li>
-        <li className='bg-blue-500 text-white p-2 m-2 rounded-lg'>Item 2</li>
-        <li className='bg-blue-500 text-white p-2 m-2 rounded-lg'>Item 3</li>
-      </ui>
+    <div className='p-4 bg-white min-h-screen dark:bg-gray-900 dark:text-white'>
+      <DarkModeToggle/>
+      <h1 className='text-2xl'>Welcome to Dark Mode App</h1>
+      <p>This is an example of implementing dark mode using Tailwind CSS</p>
     </div>
     
   )
